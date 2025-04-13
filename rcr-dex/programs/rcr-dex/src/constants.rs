80835b7cdcda7914e4948931652079883c4f0b6c
tavanam rama <rcrtavanam@gmail.com>
2025-04-14 00:40:36 +0530

    update

--- rcr-dex/programs/rcr-dex/src/constants.rs
+++
@@ -1,8 +0,0 @@
-use anchor_lang::prelude::*;
-
-#[constant]
-pub const MINIMUM_LIQUIDITY: u64 = 100; // any user have to deposit minimum of 100 liquidity
-pub const PRECISION: u64 = 10000;
-pub const SWAP_FEE_1: u64 = 10; // 0.1%
-pub const SWAP_FEE_2: u64 = 30; // 0.3%
-pub const SWAP_FEE_3: u64 = 100; // 1%
